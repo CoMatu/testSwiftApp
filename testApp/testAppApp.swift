280fc87120fa2f97b9170d2241ae608ca51fc5d9
CoMatu <30466272+CoMatu@users.noreply.github.com>
2022-09-07 17:42:54 +0300

    Initial Commit

---
+++ testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by Вячеслав Матусевич on 07.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
